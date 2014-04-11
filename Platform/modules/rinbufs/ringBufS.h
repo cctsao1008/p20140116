4f7922a2e9bd42f8f5630a9c94c329aeca6cdc55
cctsao1008 <chiacheng.tsao@gmail.com>
2014-04-11 20:53:10 +0800

    Merge functions in one file.
    Signed-off-by: cctsao1008 <chiacheng.tsao@gmail.com>

--- Platform/modules/rinbufs/ringBufS.h
+++ Platform/modules/rinbufs/ringBufS.h
@@ -25,29 +25,21 @@
 /*    THE CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR      */
 /*    PRODUCTS.                                                             */
 /****************************************************************************/
-    #define RBUF_SIZE    256
+typedef struct  ringBufS
+{
+    unsigned char *buf;
+    unsigned int buf_size;
+    int head;
+    int tail;
+    int count;
+} ringBufS;
 
+void  ringBufS_init (ringBufS *_this, unsigned char *buf, unsigned int buf_size);
+int   ringBufS_empty (ringBufS *_this);
+int   ringBufS_full  (ringBufS *_this);
+int   ringBufS_get   (ringBufS *_this);
+void  ringBufS_put   (ringBufS *_this, const unsigned char c);
+void  ringBufS_flush (ringBufS *_this, const int clearBuffer);
 
-    typedef struct  ringBufS
-    {
-      unsigned char buf[RBUF_SIZE];
-      int head;
-      int tail;
-      int count;
-    } ringBufS;
-
-    #ifdef  __cplusplus
-      extern  "C" {
-    #endif
-      void  ringBufS_init  (ringBufS *_this);
-      int   ringBufS_empty (ringBufS *_this);
-      int   ringBufS_full  (ringBufS *_this);
-      int   ringBufS_get   (ringBufS *_this);
-      void  ringBufS_put   (ringBufS *_this, const unsigned char c);
-      void  ringBufS_flush (ringBufS *_this, const int clearBuffer);
-    #ifdef  __cplusplus
-      }
-    #endif
 #endif
-
 
